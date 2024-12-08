deb2f021504a6bf3de2068ea9be9153c08d974de
ZakaUllah29 <zakaullah1421@gmail.com>
2024-12-08 13:52:37 +0500

    Initial commit

---
+++ Conditional Statement.dart
@@ -0,0 +1,31 @@
+void main(){
+String name = "Zaka";
+  int marks = 60;
+ 
+ if (marks >= 90 && marks < 100) {
+   print("A+ Grade");
+ } else if (marks >= 80 && marks < 90){
+   print("A Grade");
+   } else if (marks >= 70 && marks < 80){
+print("B+ Grade");
+    } else if (marks >= 60 && marks < 70) {
+print("B Grade");
+      } else if (marks >= 50 && marks < 60){
+print("C Grade");
+        } else if (marks >= 40 && marks < 50) {
+print("You Are Failed! Better luck next");
+          } else {
+            print("Invalid Marks. Plz Try Again.");
+            } 
+  print("$name's grade is $marks");
+
+
+ 
+
+
+
+
+
+
+
+}
---
+++ Determining Percentage.dart
@@ -0,0 +1,26 @@
+void main(){
+
+String name = "Zaka";
+num physicsmarks = 70;
+num chemistrymarks = 90;
+num mathsmarks = 80.5;
+num pstmarks = 90;
+num urdumarks = 55.9;
+num totalmarks = 500;
+num obtainedmarks = physicsmarks + chemistrymarks+ mathsmarks + pstmarks + urdumarks; 
+num percentage = obtainedmarks * 100 / totalmarks;
+
+print("$name's percentage = $percentage");
+
+
+
+
+
+
+
+
+
+
+
+
+}
